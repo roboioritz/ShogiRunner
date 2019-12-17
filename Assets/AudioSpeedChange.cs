6ed4305e5adc9edd939c6866f5ac7c2ab77123f0
Ioritz <roboioritz@yahoo.es>
2019-12-17 11:42:06 +0100

    Subir repositorio

---
+++ Assets/AudioSpeedChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSpeedChange : MonoBehaviour
+{
+    private AudioSource pista;
+    public float points;
+    public float acceleration;
+
+    void Start()
+    {
+        pista = GetComponent<AudioSource>();
+        pista.Play(0);
+        points = 1;
+    }
+    
+    void Update()
+    {
+        //Debug.Log("pich: " + pista.pitch);
+        points += acceleration * Time.deltaTime;
+        pista.pitch = points;
+    }
+}
